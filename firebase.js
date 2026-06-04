fcb329fbb1f70db704a5a42194bc6c7b7f893bb7
str717785-lab <str717785@gmail.com>
2026-06-04 19:08:54 +0600

    Refactor Firebase setup to use modular SDK
    Updated Firebase imports to use modular SDK and added analytics initialization.

--- firebase.js
+++ firebase.js
@@ -1,16 +1,22 @@
-//firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+// firebase.js
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBYNU7wTq9_1-TdIVuG-yxAlKjeFb9Rm_M",
-    authDomain: "video-cms-b7c1e.firebaseapp.com",
-    projectId: "video-cms-b7c1e",
-    storageBucket: "video-cms-b7c1e.firebasestorage.app",
-    messagingSenderId: "991575420179",
-    appId: "1:991575420179:web:23459a09aa36f868e37ee6",
-    measurementId: "G-S9R2HDC5FZ"
+  apiKey: "AIzaSyBYNU7wTq9_1-TdIVuG-yxAlKjeFb9Rm_M",
+  authDomain: "video-cms-b7c1e.firebaseapp.com",
+  projectId: "video-cms-b7c1e",
+  storageBucket: "video-cms-b7c1e.firebasestorage.app",
+  messagingSenderId: "991575420179",
+  appId: "1:991575420179:web:23459a09aa36f868e37ee6",
+  measurementId: "G-S9R2HDC5FZ"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
