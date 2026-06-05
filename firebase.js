c6b396867c7f98fa37cef2b8d871b4812f54d765
str717785-lab <str717785@gmail.com>
2026-06-05 11:52:17 +0600

    Update firebase.js file path comment

--- firebase.js
+++ firebase.js
@@ -1,4 +1,4 @@
-// js/firebase.js
+// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
